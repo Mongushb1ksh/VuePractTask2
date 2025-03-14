a0fe1a8c1d29d8c59c23cc92488cae64fedaedeb
Mongushb1ksh <bitelgra@icloud.com>
2025-03-14 14:24:46 +0700

    Шаблон Html, исправлении в app

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ let eventBus = new Vue()
 Vue.component('card-form', {
     props:{
         disabled:{
-            type: Array,
+            type: Boolean,
             required: false,
+            default: false,
         }
     },
     template: `
-        <form class="card-form" @submit.prevent="onSubmit">
+        <form class="card-form" @submit.prevent="onSubmit" :disabled="disabled">
             <p v-if="errors.length">
                 <b>Испавьте ошибки:</b>
                 <ul>
@@ -17,11 +18,11 @@ Vue.component('card-form', {
             </p>
             <p>
                 <label for="name">Заголовок</label>
-                <input id="name" v-model="name" type="text>
+                <input id="name" v-model="name" type="text">
             </p>
             <p>
                 <label for="description">Пункты </label>
-                <input id="description" v-model="description" type="text>
+                <input id="description" v-model="description" type="text">
             </p>
             <p>
                 <input type="submit" value="Добавить">
@@ -35,11 +36,12 @@ Vue.component('card-form', {
             errors: [],
         };
     },
-    method: {
+
+    methods: {
         onSubmit(){
             this.errors = [];
-            if(!this.name) this.error.push("Заполните заголовок!");
-            if(!this.description) this.error.push("Добавьте пункты!");
+            if(!this.name) this.errors.push("Заполните заголовок!");
+            if(!this.description) this.errors.push("Добавьте пункты!");
             if(this.errors.length === 0){
                 const items =this.description.split(',').map(item => ({text: item.trim(), completed: false}));
                 eventBus.$emit('add-card', {name: this.name, items});
@@ -57,15 +59,15 @@ Vue.component('card-form', {
 Vue.component('card', {
     props:{
         card:{
-            type: Array,
-            required: false,
+            type: Object,
+            required: true,
         }
     },
     template:`
         <div class="card">
             <h3>{{ card.name }}</h3>
             <ul>
-                <li>
+                <li v-for="(item, index) in card.items" :key="index">
                     <span :class="{ completed: item.completed }">{{ item.text }}</span>
                     <input type="checkbox" v-model="item.completed" @change="updateCompletion">   
                 </li>
@@ -91,35 +93,74 @@ let app = new Vue({
             ],
     },
 
-    computed: {
-
+    computed:{
+        isFirstColumnLocked(){
+            return this.columns[1].cards.length >= 5 && this.columns[0].cards.some(card => this.getCompletionPercentage(card) > 50);
+        },
     },
 
     methods: {
-        addCardToColumn() {
+        addCardToColumn(columnIndex) {
             if((columnIndex === 0 && this.columns[0].cards.length >= 3)||
                 (columnIndex === 1 && this.columns[1].cards.length >= 5)){
                     alert('Столбец переполнен! Завершите задачи');
-                    return
-                }
-                eventBus.$on('add-card', (card) => {
-                    this.columns[columnIndex].cards.push(card);
-                    this.saveData();
-                });
+                    return;
             }
+            this.columns[columnIndex].cards.push(card);
+            this.saveData();
+            
+        },
+        updateCard(columnIndex, cardIndex, updateCard){
+            const card = this.columns[columnIndex].cards[cardIndex];
+            const completionPercentage = this.getCompletionPercentage(card);
+
+            if(completionPercentage > 50 && columnIndex === 0){
+                this.moveCard(columnIndex, cardIndex, 1);
+            }else if(completionPercentage === 100 && columnIndex === 1) {
+                this.moveCard(columnIndex, cardIndex, 2);
+                card.completedAt = new Date().toLocaleString();
+            }
+
+
+            this.saveData();
+
         },
 
-        moveCard(){
+        moveCard(fromColumn, cardIndex, toColumn) {
             const card = this.columns[fromColumn].cards.splice(cardIndex, 1)[0];
+            this.columns[toColumn].cards.push(card);
         },
+
+        getCompletionPercentage(card){
+            const completed = card.items.filter(item => item.completed).length;
+            return Math.round((completed/card.items.length)*100);
+        },
+
         saveData(){
             localStorage.setItem('notesApp', JSON.stringify(this.columns));
         },
 
         loadData(){
             const savedData = localStorage.getItem('notesApp');
-            if(saveData){
+            if(savedData){
                 this.columns = JSON.parse(savedData);
-            }
+            };
+
+            this.saveData();
         },   
-})
+    },
+    created(){
+        this.loadData();
+    },
+    mounted(){
+        eventBus.$on('add-card', (card) => {
+            const columnIndex = 0; 
+            if(this.columns[columnIndex].cards.length < 3){
+                this.columns[columnIndex].cards.push(card);
+                this.saveData();
+            }else{
+                alert('Первый столбец переполнен!');
+            }
+        });
+    },
+});
